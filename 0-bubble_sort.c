d8efa3ed6404ba1c279647518094c7670b2a48e8
marwasabon <marwasabon@gmail.com>
2023-10-21 22:24:49 +0300

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+ 
+ /**
+ * bubble_sort - sortsan array for integer using the bubble sort algorithm
+ *
+ * @size: size of the array
+ * @array: is a pointer to an array
+ */
+
+ 
+void bubble_sort(int *array, size_t size)
+
+{
+    int i, n, tmp;
+    
+    bool swapped;
+    
+    if (!array || size == 0)
+	return;
+    
+    n = size - 1;
+    while (n)
+    {
+    	swapped = false;
+    	for (i = 0; i < n; i++)
+    	{
+    		tmp = array[i];
+    		if (tmp > array[i + 1])
+    		{
+    			array[i] = array[i + 1];
+    			array[i + 1] = tmp;
+    			swapped = true;
+    			print_array(array, size);
+
+
+    		}
+    	}
+    
+    	if (!swapped)
+    		break;
+    
+    	n -= 1;
+    }
+    
+} 
+ 
