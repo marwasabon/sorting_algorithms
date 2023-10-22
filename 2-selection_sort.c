96777e64c172b4fbf3688b3fde43097c2ea7d96f
marwasabon <marwasabon@gmail.com>
2023-10-22 19:51:53 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,9 +22,7 @@ void swap(int *array, size_t i, size_t j)
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int min_idx;
-	bool swapped;
+	size_t i, j, min_idx;
 
 	if (!array || size == 0)
 		return;
@@ -32,20 +30,20 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		swapped = false;
 
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
 				min_idx = j;
-				swapped = true;
 			}
 		}
 
-		if (swapped)
+		if (min_idx != i)
 		{
-			swap(array, i, min_idx);
+			int temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
 			print_array(array, size);
 		}
 	}
