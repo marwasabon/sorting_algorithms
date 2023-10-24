ab0dfebe33a0b6cb02c80e81a0a4eeb42420c450
marwasabon <marwasabon@gmail.com>
2023-10-24 22:27:49 +0000

    Task 6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -4,29 +4,16 @@
  * @array: pointer to an array
  * @size: sie of the array
  */
-
-
-To fix the errors in the code, you need to make the following changes:
-
-Add the -std=c99 flag when compiling the code to allow for loop initial declarations.
-Move the declaration of i outside of the for loop in line 13.
-Move the declaration of count to the beginning of the function, before the first for loop.
-Change the type of index from size_t to int to match the type of i in the last for loop.
-
-Here’s the updated code:
-#include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * counting_sort - sorts an array using a counting sort algorithm
- * @array: pointer to an array
- * @size: size of the array
- */
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
-	int max = array[0];
+
+	int count, max, index;
+
+	if (array == NULL || size <= 1)
+		return;
+
+	max = array[0];
 
 	for (i = 1; i < size; i++)
 	{
@@ -36,7 +23,7 @@ void counting_sort(int *array, size_t size)
 		}
 	}
 
-	int *count = (int *)malloc((max + 1) * sizeof(int));
+	count = (int *)malloc((max + 1) * sizeof(int));
 
 	for (i = 0; i <= max; i++)
 	{
@@ -56,7 +43,7 @@ void counting_sort(int *array, size_t size)
 	}
 	printf("\n");
 
-	int index = 0;
+	index = 0;
 
 	for (i = 0; i <= max; i++)
 	{
@@ -68,5 +55,5 @@ void counting_sort(int *array, size_t size)
 		}
 	}
 
-	free(count);
+	free((int)count);
 }
