b0bfbc0194e5fe528980aa215b1abf4c8806d7b3
marwasabon <marwasabon@gmail.com>
2023-10-25 01:35:30 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -6,52 +6,43 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	size_t i;
-	int *count;
-	size_t max = 0;
-	int index = 0;
+    size_t i;
 
-	if (array == NULL || size <= 1)
-		return;
+    int *count, max, index;
 
-	for (i = 0; i < size; i++)
-	{
-		if ((size_t)array[i] > max)
-		{
-			max = (size_t)array[i];
-		}
-	}
+    max = array[0];
 
-	count = malloc((max + 1) * sizeof(int));
-	if (count == NULL)
-		return;
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
 
-	for (i = 0; i <= max; i++)
-	{
-		count[i] = 0;
-	}
+    count = (int *)malloc((max + 1) * sizeof(int));
 
-	for (i = 0; i < size; i++)
-	{
-		count[array[i]]++;
-	}
+    for (i = 0; i <= max; i++)
+    {
+        count[i] = 0;
+    }
 
-	printf("Counting array: ");
-	for (i = 0; i <= max; i++)
-	{
-		printf("%d ", count[i]);
-	}
-	printf("\n");
+    for (i = 0; i < size; i++)
+    {
+        count[array[i]]++;
+    }
 
-	for (i = 0; i <= max; i++)
-	{
-		while (count[i] > 0)
-		{
-			array[index] = i;
-			index++;
-			count[i]--;
-		}
-	}
+    index = 0;
 
-	free(count);
+    for (i = 0; i <= max; i++)
+    {
+        while (count[i] > 0)
+        {
+            array[index] = i;
+            index++;
+            count[i]--;
+        }
+    }
+
+    free(count);
 }
