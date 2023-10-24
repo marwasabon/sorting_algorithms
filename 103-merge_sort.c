53b379528f1a8bfe0b15a69959c31e9d06231418
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-24 13:29:00 +0100

    Remove unnecessary comments

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,12 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* void _sort_recursively(int *arr, int *cp_arr, */
-/* 					   size_t i_start, size_t i_end, size_t size); */
-/* void _merge(int *arr, int *cp_arr, */
-/* 					   size_t i_sta, size_t i_end, size_t i_mid, size_t size); */
-/* void _copy(int *src, int *dest, size_t src_size); */
-
 void _merge(int *s_arr, int *d_arr, size_t start, size_t end, size_t middle)
 {
 	size_t i, j, k;
@@ -61,28 +55,4 @@ void merge_sort(int *array, size_t size)
 
 
 	_divide(cpy_array, 0, size, array);
-	/* cpy_array = malloc(sizeof(int) * size); */
-	/* _copy(array, cpy_array, size); */
-	/* _sort_recursively(array, cpy_array, 0, size, size); */
-	/* print_array(cpy_array, size); */
-	/* free(cpy_array); */
 }
-
-
-
-/* void _sort_recursively(int *arr, int *cp_arr, */
-/* 					   size_t i_sta, size_t i_end, size_t size) */
-/* { */
-/* 	int i_mid; */
-
-/* 	if ((i_end - i_sta) <= 1) */
-/* 		return; */
-
-/* 	i_mid = (i_sta + i_end) / 2; */
-
-/* 	_sort_recursively(arr, cp_arr, i_sta, i_mid, size); */
-/* 	_sort_recursively(arr, cp_arr, i_mid, i_end, size); */
-
-/* 	_merge(cp_arr, arr, i_sta, i_end, i_mid, size); */
-/* } */
-
