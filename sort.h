84f288850ded50461aab625e74ca6dff9a74be50
marwasabon <marwasabon@gmail.com>
2023-10-25 00:58:35 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -28,4 +28,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void _sort(int *array, int low, int high, size_t size);
 int _partition(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif
